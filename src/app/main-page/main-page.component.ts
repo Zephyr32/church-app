140d367c24fe0bd7cbbf1dec196a2bd351d96801
Bogdan <35693010+Zephyr32@users.noreply.github.com>
2020-11-10 16:40:57 +0200

    add pritch

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import{Pritchi} from "./Pritchi";
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {
+this.pritcha=this.pritch.getRandompritch();
+  }
+   public pritch:Pritchi=new Pritchi();
+   pritcha:string;
   ngOnInit(): void {
   }
+
+  
 
 }
